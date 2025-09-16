15162dd5c949719ecc91ee760800bf8b3dcef81f
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-16 22:04:04 +0530

    Update RecommendedMovies.jsx

--- src/Components/RecommendedMovies/RecommendedMovies.jsx
+++ src/Components/RecommendedMovies/RecommendedMovies.jsx
@@ -30,7 +30,7 @@ const RecommendedMovies = () => {
         genre: "Drama/Family",
         rating: "8.3/10",
         votes: "434.6K Votes",
-        image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-MS4zSyBMaWtlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00424440-fypnayhzqq-portrait.jpg",
+        image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/mom-tane-nai-samjay-et00424440-1734259829.jpg",
       },
       {
         title: "MARCO",
