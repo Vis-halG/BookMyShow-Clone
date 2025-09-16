6a9d8b06cb3614837c7f76c34578c89b8ede9137
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-16 21:58:46 +0530

    Update RecommendedMovies.jsx

--- src/Components/RecommendedMovies/RecommendedMovies.jsx
+++ src/Components/RecommendedMovies/RecommendedMovies.jsx
@@ -17,7 +17,7 @@ const RecommendedMovies = () => {
         genre: "Action/Drama/political",
         rating: "8.3/10",
         votes: "434.6K Votes",
-        image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-NjU0LjlLIExpa2Vz,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00311772-rnlqghebfk-portrait.jpg",
+        image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/game-changer-et00311772-1731311322.jpg",
       },
       {
         title: "Pushpa 2: The Rule",
