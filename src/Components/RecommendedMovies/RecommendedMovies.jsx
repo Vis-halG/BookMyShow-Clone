0b6eb68e63113da7f7fdfce2db232f3217d9e715
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-16 21:59:20 +0530

    Update RecommendedMovies.jsx

--- src/Components/RecommendedMovies/RecommendedMovies.jsx
+++ src/Components/RecommendedMovies/RecommendedMovies.jsx
@@ -10,7 +10,7 @@ const RecommendedMovies = () => {
       genre: "Action/Thriller",
       rating: "8.3/10",
       votes: "434.6K Votes",
-      image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-NDguM0sgTGlrZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00391731-pszykzmtjn-portrait.jpg",
+      image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/fateh-et00391731-1734092649.jpg",
     },
     {
         title: "GAME CHANGER",
