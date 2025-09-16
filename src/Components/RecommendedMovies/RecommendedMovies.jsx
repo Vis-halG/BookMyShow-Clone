4d2f3d8b92c53785989c5050f48d720a3700bda0
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-16 22:04:44 +0530

    Update RecommendedMovies.jsx

--- src/Components/RecommendedMovies/RecommendedMovies.jsx
+++ src/Components/RecommendedMovies/RecommendedMovies.jsx
@@ -37,7 +37,7 @@ const RecommendedMovies = () => {
         genre: "Action/Thriller",
         rating: "8.3/10",
         votes: "434.6K Votes",
-        image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC42LzEwICA5Mi4zSyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00416394-jmmlluuywc-portrait.jpg",
+        image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/marco-et00416394-1734329568.jpg",
       },
       {
         title: "NOSFERATU",
